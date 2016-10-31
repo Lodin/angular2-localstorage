d10e1c1a6c07b8f845a03b18a977b7ff5268af62
Vlad Rindevich <rindevich.vs@gmail.com>
2016-10-31 16:25:53 +0300

    chore(test): add necessary polyfills

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,22 @@
 /* tslint:disable */
 
-import 'core-js/es6';
-import 'core-js/es7/array';
+import 'ts-helpers';
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'core-js/es6/reflect';
 
+import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
@@ -19,7 +33,6 @@ Promise.all([
   System.import('@angular/core/testing'),
   System.import('@angular/platform-browser-dynamic/testing')
 ])
-// First, initialize the Angular testing environment.
   .then(([testing, testingBrowser]) => {
     testing.getTestBed().initTestEnvironment(
       testingBrowser.BrowserDynamicTestingModule,
@@ -27,6 +40,6 @@ Promise.all([
     );
   })
   .then(() => require.context('./', true, /\.spec\.ts/))
-  .then((context: any) => context.keys().map(context))
+  .then(context => context.keys().map(context))
   .then(__karma__.start, __karma__.error);
 
